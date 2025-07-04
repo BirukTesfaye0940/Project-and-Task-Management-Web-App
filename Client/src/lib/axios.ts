d03c00aa59a0abe027c11b89f954afcd0701b6a0
Biruk_Tesfaye <biruktesfayeakabu@gmail.com>
2025-07-04 10:46:57 +0300

    Update axios.ts

--- Client/src/lib/axios.ts
+++ Client/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     //baseURL: "http://localhost:5003/api",
-    baseURL: "https://project-and-task-management-web-app.onrender.com",
+    baseURL: "https://project-and-task-management-web-app.onrender.com/api",
     withCredentials: true,
 })
